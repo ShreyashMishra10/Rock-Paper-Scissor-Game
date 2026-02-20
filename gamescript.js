8a4dea18450bc66d273a690354002d08d12bff89
ShreyashMishra10 <shreyashmishra9044@gmail.com>
2026-02-20 18:53:51 +0530

    finalised the mini project with better ui by adding css and rock-paper-scissor images

--- gamescript.js
+++ gamescript.js
@@ -14,31 +14,32 @@ let score =JSON.parse(localStorage.getItem('score')) || {
  }
         */
 function play(pmove) {
+    pmove = pmove.toLowerCase();
     const computerMove = move();
     let result = '';
 
-    if (pmove === 'Scissors') {
-        if (computerMove === 'Computer chose Rock') {
+    if (pmove === 'scissors') {
+        if (computerMove === 'rock') {
             result = 'You lose!';
-        } else if (computerMove === 'Computer chose Paper') {
+        } else if (computerMove === 'paper') {
             result = 'You win!';
-        } else if (computerMove === 'Computer chose Scissors') {
+        } else if (computerMove === 'scissors') {
             result = 'Tie!';
         }
-    } else if (pmove === 'Paper') {
-        if (computerMove === 'Computer chose Rock') {
+    } else if (pmove === 'paper') {
+        if (computerMove === 'rock') {
             result = 'You win!';
-        } else if (computerMove === 'Computer chose Paper') {
+        } else if (computerMove === 'paper') {
             result = 'Tie!'; 
-        } else if (computerMove === 'Computer chose Scissors') {
+        } else if (computerMove === 'scissors') {
             result = 'You lose!';
         }
-    } else if (pmove === 'Rock') {
-        if (computerMove === 'Computer chose Rock') {
+    } else if (pmove === 'rock') {
+        if (computerMove === 'rock') {
             result = 'Tie!';
-        } else if (computerMove === 'Computer chose Paper') {
+        } else if (computerMove === 'paper') {
             result = 'You lose!';
-        } else if (computerMove === 'Computer chose Scissors') {
+        } else if (computerMove === 'scissors') {
             result = 'You win!';
         }
     }
@@ -54,7 +55,7 @@ function play(pmove) {
     updateScore();
 
     document.querySelector('.js-result').innerHTML = result;
-    document.querySelector('.js-moves').innerHTML = `You picked ${pmove}\n${computerMove}`;
+    document.querySelector('.js-moves').innerHTML = `You<img src="images/${pmove}-emoji.png" class="move-icon"> <img src="images/${computerMove}-emoji.png" class="move-icon">Computer`;
  
     // remove the alert and replace it with the above two lines to display the result and moves on the webpage instead of an alert box.
 
@@ -69,11 +70,11 @@ function move() {
     const rand = Math.random();
     let computerMove = '';
     if (rand >= 0 && rand < 1/3) {
-        computerMove = 'Computer chose Rock';
+        computerMove = 'rock';
     } else if (rand >= 1/3 && rand < 2/3) {
-        computerMove = 'Computer chose Paper';
+        computerMove = 'paper';
     } else if (rand >= 2/3 && rand < 1) {
-        computerMove = 'Computer chose Scissors';
+        computerMove = 'scissors';
     }
     return computerMove;
 }
